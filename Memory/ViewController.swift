6bc54766641215b39ee1e3702a50ef67878869ad
Seung Hyun Eun <seunghyuneun@Seungs-MacBook-Pro-2.local>
2018-04-08 18:24:56 -0700

    Initial Commit / Finalize the Design / Kind of... Randomize the Emojis / Kind of... Make the UIAlert Pop

---
+++ Memory/ViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  Memory
+//
+//  Created by Sunny Eun on 4/3/18.
+//  Copyright © 2018 Sunny Eun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    var symbols = [
+        "😉",
+        "🤨",
+        "😉",
+        "🤨"
+    ]
+    
+    var selectedCard: CardButton?
+    
+
+    
+    //When a card is flipped
+    @IBAction func flipCard(_ sender: CardButton) {
+        
+        // Randomize the symbols.
+        let symbol = symbols[Int(arc4random() % UInt32(sender.tag))]
+        let senderTitle = sender.title(for: .normal)
+        
+        if senderTitle == nil || senderTitle == "" {
+            if let card = selectedCard {
+                
+                // If tapped card is same symbol as already flipped one, then flip both.
+                if card.title(for: .normal) == symbol {
+                    sender.setTitle(symbol, for: .normal)
+                    
+                    // UIAlert for when all cards are matched.
+                    let alertController = UIAlertController(title: "Great Job!", message: "You matched all cards.", preferredStyle: .alert)
+                    
+                    let OKButton = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) { (action:UIAlertAction) in print("You've pressed OK");
+                    }
+                    
+                    // UIAlert with OK button pop up.
+                    alertController.addAction(OKButton)
+                    self.present(alertController, animated: true, completion: nil)
+                    
+                } else { // Otherwise, we flip both back.
+                    card.setTitle("", for: .normal)
+     
+                }
+              
+                selectedCard = nil
+
+            } else { // If we didn't have any flipped card, flip it and assign it to selectedCard.
+                sender.setTitle(symbol, for: .normal)
+                selectedCard = sender
+                
+            }
+        }
+    }
+}
+
+
+
+
+
